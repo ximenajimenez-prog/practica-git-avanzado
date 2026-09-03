c85041b78da87de9648c4eea8d2b8edbac6354b9
Ximenajimenez-prog <milyjimenez8809@gmail.com>
2026-09-03 16:50:32 -0500

    feat: agregar funcion dividir

--- calculadora.py
+++ calculadora.py
@@ -10,7 +10,13 @@ def restar(a, b):
 def multiplicar(a, b):
     return a * b
 
+# ============================================
+# CALCULADORA - Funciones adicionales
+# Agreguen estas funciones a su calculadora.py,
+# una por una, con su propio commit para cada una
+# ============================================
+
 def dividir(a, b):
     if b == 0:
-        raise ValueError("No se puede dividir entre cero.")
+        return "Error: no se puede dividir entre cero"
     return a / b
